27f2eb45999d550deafa8d6e3bc123c731133638
Mlody05 <dmatecki77@googlemail.com>
2026-02-25 19:11:46 +0000

    Update serwer.js

--- serwer.js
+++ serwer.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const path = require('path');
-// ... reszta kodu serwera ...
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serwowanie plików statycznych (Twojego index.html)
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Serwer NEXITY dla Nikolety działa na porcie ${port}`);
+});
